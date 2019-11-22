8ec69bca4058270ad3a2cf63474bccc224f945ce
basvdbrink1 <tsdutch@hotmail.com>
2019-11-22 11:38:17 +0100

    begin animaties homescreen

---
+++ assets/js/home.js
@@ -0,0 +1,16 @@
+TweenMax.from('.introduction-header h1',1,{
+    y: -300, opacity: 0, scale: 0.5
+})
+
+TweenMax.from('.introduction-header h2',0.5,{
+    delay:1,
+    y:100,
+    rotation:90,
+    opacity:0
+})
+
+TweenMax.from('#sass-logo',0.5,{
+    delay:1.5,
+    x: 2000,
+    duration:2,
+})
