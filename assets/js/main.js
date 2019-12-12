f074d7110e702f76a1a7b303274bfdbcde9dbb5d
basvdbrink1 <tsdutch@hotmail.com>
2019-12-12 21:06:17 +0100

    gsap merge

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
-TweenMax.to(".logo", 6, {
-    x:600,
-    rotation:360,
+TweenMax.to(".gsap-logo1", 1.5, {
+    x:-900,
+    rotation:-360,
     
 });
+TweenMax.to(".gsap-logo1", 1.5, {
+    x:0,
+    rotation:-720,
+    delay:1,    
+    
+});
+
